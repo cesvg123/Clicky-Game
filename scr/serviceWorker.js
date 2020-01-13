266aecf80012ed4ba8948f09d53ad33366d38128
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2020-01-13 06:27:17 -0500

    added prettier formatting

--- scr/serviceWorker.js
+++ scr/serviceWorker.js
@@ -2,95 +2,95 @@ var localhost = Boolean(
     window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
     window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
-  );
+);
 
-  export function register(config) {
+export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
 
-      const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-      if (publicUrl.origin !== window.location.origin) {
-        return;
-      }
-      window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-  
-        if (localhost) {
-          checkValidServiceWorker(swUrl, config);
-          navigator.serviceWorker.ready.then(() => {
-            console.log(
-              'Working --- '
-            );
-          });
-        } else {
-          registerValidSW(swUrl, config);
-        }
-      });
-    }
-  }
-  
-  function registerValidSW(swUrl, config) {
-    navigator.serviceWorker
-      .register(swUrl)
-      .then(registration => {
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          if (installingWorker == null) {
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+        if (publicUrl.origin !== window.location.origin) {
             return;
-          }
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === 'installed') {
-              if (navigator.serviceWorker.controller) {
-                console.log(
-                  'Working with new content ' 
-                );
-                if (config && config.onUpdate) {
-                  config.onUpdate(registration);
-                }
-              } else {
-                console.log('could work offline');
-                if (config && config.onSuccess) {
-                  config.onSuccess(registration);
-                }
-              }
-            }
-          };
-        };
-      })
-      .catch(error => {
-        console.error('Error', error);
-      });
-  }
-  
-  function checkValidServiceWorker(swUrl, config) {
-    fetch(swUrl)
-      .then(response => {
-        const contentType = response.headers.get('content-type');
-        if (
-          response.status === 404 ||
-          (contentType != null && contentType.indexOf('javascript') === -1)
-        ) {
-          navigator.serviceWorker.ready.then(registration => {
-            registration.unregister().then(() => {
-              window.location.reload();
-            });
-          });
-        } else {
-          registerValidSW(swUrl, config);
         }
-      })
-      .catch(() => {
-        console.log(
-          'App is running in offline mode/no internet connectivity'
-        );
-      });
-  }
-  
-  export function unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.unregister();
-      });
+        window.addEventListener('load', () => {
+            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+            if (localhost) {
+                checkValidServiceWorker(swUrl, config);
+                navigator.serviceWorker.ready.then(() => {
+                    console.log(
+                        'Working --- '
+                    );
+                });
+            } else {
+                registerValidSW(swUrl, config);
+            }
+        });
     }
-  }
+}
+
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+        .register(swUrl)
+        .then(registration => {
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                if (installingWorker == null) {
+                    return;
+                }
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            console.log(
+                                'Working with new content '
+                            );
+                            if (config && config.onUpdate) {
+                                config.onUpdate(registration);
+                            }
+                        } else {
+                            console.log('could work offline');
+                            if (config && config.onSuccess) {
+                                config.onSuccess(registration);
+                            }
+                        }
+                    }
+                };
+            };
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });
+}
+
+function checkValidServiceWorker(swUrl, config) {
+    fetch(swUrl)
+        .then(response => {
+            const contentType = response.headers.get('content-type');
+            if (
+                response.status === 404 ||
+                (contentType != null && contentType.indexOf('javascript') === -1)
+            ) {
+                navigator.serviceWorker.ready.then(registration => {
+                    registration.unregister().then(() => {
+                        window.location.reload();
+                    });
+                });
+            } else {
+                registerValidSW(swUrl, config);
+            }
+        })
+        .catch(() => {
+            console.log(
+                'App is running in offline mode/no internet connectivity'
+            );
+        });
+}
+
+export function unregister() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then(registration => {
+            registration.unregister();
+        });
+    }
+}
