c0a49250d572a80b9832407630c3eb3fabe0b776
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2020-01-14 18:33:02 -0500

    updating bootstrap

--- my-app/src/components/cardItem/cardItem.js
+++ my-app/src/components/cardItem/cardItem.js
@@ -2,17 +2,9 @@ import React from 'react';
 import './CardItem.css';
 
 var CardItem = props => (
-  <div
-    onClick={() => props.imageClick(props.movie.id)}
-    className='card-box col-4 col-xs-4 col-sm-4 col-md-4 col-lg-2'
-  >
+  <div onClick={() => props.imageClick(props.movie.id)} className="col-md-3">
     <div className='img-container'>
-      <img
-        title={props.movie.name}
-        alt={props.movie.name}
-        src={props.movie.image}
-        className='img-thumbnail'
-      />
+      <img title={props.movie.name} alt={props.movie.name} src={props.movie.image} className='img-thumbnail'/>
     </div>
   </div>
 );
