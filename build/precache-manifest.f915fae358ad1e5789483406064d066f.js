b1f58f6250f618897e8e86c23503769fbe02fb9f
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2020-01-14 20:19:09 -0500

    correcting errors

---
+++ build/precache-manifest.f915fae358ad1e5789483406064d066f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4018e6279065927f69ed31c41906593f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4a57d3000d18ffbb1906",
+    "url": "/Clicky-Game/static/css/main.5f3b053f.chunk.css"
+  },
+  {
+    "revision": "503d89b3b7ff06bf56e0",
+    "url": "/Clicky-Game/static/js/2.1cce1e1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.1cce1e1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a57d3000d18ffbb1906",
+    "url": "/Clicky-Game/static/js/main.7dcf91f7.chunk.js"
+  },
+  {
+    "revision": "0c44dba98bd5877c6a80",
+    "url": "/Clicky-Game/static/js/runtime-main.297251f5.js"
+  }
+]);
